6345b8326ec709905483dddf33c0a6783e64b38c
LunarDiver <25Prozent@gmail.com>
2021-04-07 19:58:41 +0200

    Added default urls.

--- Sibilus.Web/Program.cs
+++ Sibilus.Web/Program.cs
@@ -21,6 +21,7 @@ namespace Sibilus.Web
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:80", "https://*:443");
                 });
     }
 }
