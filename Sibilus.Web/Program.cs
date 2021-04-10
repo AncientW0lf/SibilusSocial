7abddeb05b71ba2d913d9135e042198f578b439b
LunarDiver <25Prozent@gmail.com>
2021-04-10 12:48:20 +0200

    Added disposing of SessionsClearer timer.

--- Sibilus.Web/Program.cs
+++ Sibilus.Web/Program.cs
@@ -36,6 +36,7 @@ namespace Sibilus.Web
         private static void ExitApplication()
         {
             DbCache.DbClient?.Dispose();
+            DbCache.SessionClearer?.Dispose();
         }
     }
 }
